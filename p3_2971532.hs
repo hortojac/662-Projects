bf4a355215937be9aaefd83736e8acbbb72617f9
Jacob Horton <jacob.horton@ku.edu>
2023-04-09 10:48:52 -0500

    Initial

--- p3_2971532.hs
+++ p3_2971532.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GADTs,FlexibleContexts #-}
 
-
 -- AST and Type Definitions
 
 data KUTypeLang where
@@ -9,18 +8,18 @@ data KUTypeLang where
     deriving (Show,Eq)
 
 data KULang where
-    Num :: Int -> KULang  
-    Plus :: KULang -> KULang -> KULang 
+    Num :: Int -> KULang
+    Plus :: KULang -> KULang -> KULang
     Minus :: KULang -> KULang -> KULang
-    Mult :: KULang -> KULang -> KULang 
-    Div :: KULang -> KULang -> KULang  
+    Mult :: KULang -> KULang -> KULang
+    Div :: KULang -> KULang -> KULang
     Exp :: KULang -> KULang -> KULang
-    Boolean :: Bool -> KULang  
-    And :: KULang -> KULang -> KULang 
-    Or :: KULang -> KULang -> KULang  
-    Leq :: KULang -> KULang -> KULang  
-    IsZero :: KULang -> KULang  
-    If :: KULang -> KULang -> KULang -> KULang 
+    Boolean :: Bool -> KULang
+    And :: KULang -> KULang -> KULang
+    Or :: KULang -> KULang -> KULang
+    Leq :: KULang -> KULang -> KULang
+    IsZero :: KULang -> KULang
+    If :: KULang -> KULang -> KULang -> KULang
     Between :: KULang -> KULang -> KULang -> KULang
     Bind :: String -> KULang -> KULang -> KULang
     Id :: String -> KULang
@@ -38,24 +37,173 @@ type Cont = [(String,KUTypeLang)]
 -- Part 1: Adding Booleans
 
 -- Exercise 1
-evalDirect :: KULang -> (Maybe KULang)
-evalDirect _ = Nothing
+evalDirect :: KULang -> Maybe KULang
+evalDirect (Num x) = if x<0 then Nothing else Just (Num x)
+evalDirect (Plus x y) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    return (Num (x'+y'))
+evalDirect (Minus x y) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    if x'-y'<0 then Nothing else return (Num (x'-y'))
+evalDirect (Mult x y) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    return (Num (x'*y'))
+evalDirect (Div x y) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    if y'==0 then Nothing else return (Num (x'`div`y'))
+evalDirect (Exp x y) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    if y'<0 then Nothing else return (Num (x'^y'))
+evalDirect (Boolean x) = Just (Boolean x)
+evalDirect (And x y) = do
+    (Boolean x') <- evalDirect x
+    (Boolean y') <- evalDirect y
+    return (Boolean (x'&&y'))
+evalDirect (Or x y) = do
+    (Boolean x') <- evalDirect x
+    (Boolean y') <- evalDirect y
+    return (Boolean (x'||y'))
+evalDirect (Leq x y) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    return (Boolean (x'<=y'))
+evalDirect (IsZero x) = do
+    (Num x') <- evalDirect x
+    if x'==0 then return (Boolean True) else return (Boolean False)
+evalDirect (If x y z) = do
+    (Boolean x') <- evalDirect x
+    if x' then return y else return z
+evalDirect (Between x y z) = do
+    (Num x') <- evalDirect x
+    (Num y') <- evalDirect y
+    (Num z') <- evalDirect z
+    if x'<=y' && y'<=z' then return (Boolean True) else return (Boolean False)
+evalDirect (Bind x y z) = do
+    y' <- evalDirect y
+    return (Bind x y' z)
+evalDirect (Id x) = Nothing
 
 -- Exercise 2
-evalDeferred :: Env -> KULang -> (Maybe KULang)
-evalDeferred _ _ = Nothing
+evalDeferred :: Env -> KULang -> Maybe KULang
+evalDeferred _ (Num x) = if x<0 then Nothing else Just (Num x)
+evalDeferred env (Plus x y) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    return (Num (x'+y'))
+evalDeferred env (Minus x y) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    if x'-y'<0 then Nothing else return (Num (x'-y'))
+evalDeferred env (Mult x y) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    return (Num (x'*y'))
+evalDeferred env (Div x y) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    if y'==0 then Nothing else return (Num (x'`div`y'))
+evalDeferred env (Exp x y) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    if y'<0 then Nothing else return (Num (x'^y'))
+evalDeferred _ (Boolean x) = Just (Boolean x)
+evalDeferred env (And x y) = do
+    (Boolean x') <- evalDeferred env x
+    (Boolean y') <- evalDeferred env y
+    return (Boolean (x'&&y'))
+evalDeferred env (Or x y) = do
+    (Boolean x') <- evalDeferred env x
+    (Boolean y') <- evalDeferred env y
+    return (Boolean (x'||y'))
+evalDeferred env (Leq x y) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    return (Boolean (x'<=y'))
+evalDeferred env (IsZero x) = do
+    (Num x') <- evalDeferred env x
+    if x'==0 then return (Boolean True) else return (Boolean False)
+evalDeferred env (If x y z) = do
+    (Boolean x') <- evalDeferred env x
+    if x' then return y else return z
+evalDeferred env (Between x y z) = do
+    (Num x') <- evalDeferred env x
+    (Num y') <- evalDeferred env y
+    (Num z') <- evalDeferred env z
+    if x'<=y' && y'<=z' then return (Boolean True) else return (Boolean False)
+evalDeferred env (Bind x y z) = do
+    y' <- evalDeferred env y
+    return (Bind x y' z)
+evalDeferred env (Id x) = lookup x env
 
 -- Exercise 3
 testEvals :: KULang -> Bool
-testEvals _ = True
+testEvals x = evalDirect x == evalDeferred [] x
 
 -- Part 2: Type Checking
 
 --Exercise 1
-typeofMonad :: Cont -> KULang -> (Maybe KUTypeLang)
-typeofMonad _ _ = Nothing
+typeofMonad :: Cont -> KULang -> Maybe KUTypeLang
+typeofMonad _ (Num x) = if x<0 then Nothing else return TNum
+typeofMonad cont (Plus x y) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    return TNum
+typeofMonad cont (Minus x y) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    return TNum
+typeofMonad cont (Mult x y) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    return TNum
+typeofMonad cont (Div x y) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    return TNum
+typeofMonad cont (Exp x y) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    return TNum
+typeofMonad _ (Boolean x) = return TBool
+typeofMonad cont (And x y) = do
+    TBool <- typeofMonad cont x
+    TBool <- typeofMonad cont y
+    return TBool
+typeofMonad cont (Or x y) = do
+    TBool <- typeofMonad cont x
+    TBool <- typeofMonad cont y
+    return TBool
+typeofMonad cont (Leq x y) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    return TBool
+typeofMonad cont (IsZero x) = do
+    TNum <- typeofMonad cont x
+    return TBool
+typeofMonad cont (If x y z) = do
+    TBool <- typeofMonad cont x
+    y' <- typeofMonad cont y
+    z' <- typeofMonad cont z
+    if y'==z' then return y' else Nothing
+typeofMonad cont (Between x y z) = do
+    TNum <- typeofMonad cont x
+    TNum <- typeofMonad cont y
+    TNum <- typeofMonad cont z
+    return TBool
+typeofMonad cont (Bind x y z) = do
+    y' <- typeofMonad cont y
+    typeofMonad ((x,y'):cont) z
+typeofMonad cont (Id x) = lookup x cont
 
 --Exercise 2
-interpret :: KULang -> (Maybe KULang)
-interpret _ = Nothing
+interpret :: KULang -> Maybe KULang
+interpret x = do
+    _ <- typeofMonad [] x
+    evalDeferred [] x
+
 
---
+++ p4_2971532.hs
@@ -0,0 +1,179 @@
+{-# LANGUAGE GADTs,FlexibleContexts #-}
+{-# OPTIONS_GHC -Wno-missing-methods #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# OPTIONS_GHC -Wno-noncanonical-monad-instances #-}
+
+-- Abstract Syntax Definitions
+data KULang where
+    Num :: Int -> KULang
+    Plus :: KULang -> KULang -> KULang
+    Minus :: KULang -> KULang -> KULang
+    Mult :: KULang -> KULang -> KULang
+    Div :: KULang -> KULang -> KULang
+    Exp :: KULang -> KULang -> KULang
+    If0 :: KULang -> KULang -> KULang -> KULang
+    Id :: String -> KULang
+    Lambda :: String -> KULang -> KULang
+    App :: KULang -> KULang -> KULang
+    deriving (Show,Eq)
+
+data KULangVal where
+    NumV :: Int -> KULangVal
+    ClosureV :: String -> KULang -> EnvVal -> KULangVal
+    deriving (Show,Eq)
+
+data KULangExt where
+    NumX :: Int -> KULangExt
+    PlusX :: KULangExt -> KULangExt -> KULangExt
+    MinusX :: KULangExt -> KULangExt -> KULangExt
+    MultX :: KULangExt -> KULangExt -> KULangExt
+    DivX :: KULangExt -> KULangExt -> KULangExt
+    ExpX :: KULangExt -> KULangExt -> KULangExt
+    If0X :: KULangExt -> KULangExt -> KULangExt -> KULangExt
+    LambdaX :: String -> KULangExt -> KULangExt
+    AppX :: KULangExt -> KULangExt -> KULangExt
+    BindX :: String -> KULangExt -> KULangExt -> KULangExt
+    IdX :: String -> KULangExt
+    deriving (Show,Eq)
+
+-- Environment Definitions
+type Env = [(String,KULang)]
+type EnvVal = [(String,KULangVal)]
+
+-- Reader Definition
+data Reader e a = Reader (e -> a)
+
+-- Monad Definition
+instance Monad (Reader e) where
+ return x = Reader $ const x
+ g >>= f = Reader $ \e -> runR (f (runR g e)) e
+
+ -- Applicative Definition
+instance Applicative (Reader e) where
+ pure x = Reader $ const x
+(Reader f) <*> (Reader g) = Reader $ \e -> f e (g e)
+
+-- Functor Definition
+instance Functor (Reader e) where
+ fmap f (Reader g) = Reader $ \e -> (f . g) e
+
+-- Fail Definition
+instance MonadFail (Reader e) where
+        fail = error "fail"
+
+-- Helper Methods
+runR :: Reader e a -> e -> a
+runR (Reader f) = f
+
+ask :: Reader a a
+ask = Reader id
+
+local :: (e->t) -> Reader t a -> Reader e a
+local f r = ask >>= \e -> return (runR r (f e))
+
+useClosure :: String -> KULangVal -> EnvVal -> EnvVal -> EnvVal
+useClosure i v e _ = (i,v):e
+
+
+-----------------------------
+----- Project Exercises -----
+-----------------------------
+
+-- Part 1: Scoping
+
+-- Exercise 1:
+evalDyn :: Env -> KULang -> Maybe KULang
+evalDyn e (Num x) = Just (Num x)
+evalDyn e (Plus x y) = do
+    Num x' <- evalDyn e x
+    Num y' <- evalDyn e y
+    return (Num (x' + y'))
+evalDyn e (Minus x y) = do
+    Num x' <- evalDyn e x
+    Num y' <- evalDyn e y
+    return (Num (x' - y'))
+evalDyn e (Mult x y) = do
+    Num x' <- evalDyn e x
+    Num y' <- evalDyn e y
+    return (Num (x' * y'))
+evalDyn e (Div x y) = do
+    Num x' <- evalDyn e x
+    Num y' <- evalDyn e y
+    return (Num (x' `div` y'))
+evalDyn e (Exp x y) = do
+    Num x' <- evalDyn e x
+    Num y' <- evalDyn e y
+    return (Num (x' ^ y'))
+evalDyn e (If0 x y z) = do
+    Num x' <- evalDyn e x
+    if x' == 0 then evalDyn e y else evalDyn e z
+evalDyn e (Lambda x y) = Just (Lambda x y)
+evalDyn e (App x y) = do
+    Lambda x' y' <- evalDyn e x
+    z <- evalDyn e y
+    evalDyn ((x',z):e) y'
+evalDyn e (Id x) = lookup x e
+
+-- Exercise 2:
+evalStat :: EnvVal -> KULang -> Maybe KULangVal
+evalStat e (Num x) = Just (NumV x)
+evalStat e (Plus x y) = do
+    NumV x' <- evalStat e x
+    NumV y' <- evalStat e y
+    return (NumV (x' + y'))
+evalStat e (Minus x y) = do
+    NumV x' <- evalStat e x
+    NumV y' <- evalStat e y
+    return (NumV (x' - y'))
+evalStat e (Mult x y) = do
+    NumV x' <- evalStat e x
+    NumV y' <- evalStat e y
+    return (NumV (x' * y'))
+evalStat e (Div x y) = do
+    NumV x' <- evalStat e x
+    NumV y' <- evalStat e y
+    return (NumV (x' `div` y'))
+evalStat e (Exp x y) = do
+    NumV x' <- evalStat e x
+    NumV y' <- evalStat e y
+    return (NumV (x' ^ y'))
+evalStat e (If0 x y z) = do
+    NumV x' <- evalStat e x
+    if x' == 0 then evalStat e y else evalStat e z
+evalStat e (Lambda x y) = Just (ClosureV x y e)
+evalStat e (App x y) = do
+    ClosureV x' y' e' <- evalStat e x
+    z <- evalStat e y
+    evalStat (useClosure x' z e' e) y'
+evalStat e (Id x) = lookup x e
+
+-- Part 2: Elaboration
+
+-- Exercise 3:
+elabTerm :: KULangExt -> KULang
+elabTerm (NumX x) = Num x
+elabTerm (PlusX x y) = Plus (elabTerm x) (elabTerm y)
+elabTerm (MinusX x y) = Minus (elabTerm x) (elabTerm y)
+elabTerm (MultX x y) = Mult (elabTerm x) (elabTerm y)
+elabTerm (DivX x y) = Div (elabTerm x) (elabTerm y)
+elabTerm (ExpX x y) = Exp (elabTerm x) (elabTerm y)
+elabTerm (If0X x y z) = If0 (elabTerm x) (elabTerm y) (elabTerm z)
+elabTerm (LambdaX x y) = Lambda x (elabTerm y)
+elabTerm (AppX x y) = App (elabTerm x) (elabTerm y)
+elabTerm (BindX x y z) = App (Lambda x (elabTerm z)) (elabTerm y)
+elabTerm (IdX x) = Id x
+
+-- Exercise 4:
+interpElab :: EnvVal -> KULangExt -> Maybe KULangVal
+interpElab e x = evalStat e (elabTerm x)
+
+-- Part 3: Reader Monad
+
+-- Exercise 5:
+evalReader :: KULang -> Reader EnvVal KULangVal
+evalReader (Num x) = return (NumV x)
+
+-- Exercise 6:
+interpReader :: KULangExt -> KULangVal
+interpReader x = runR (evalReader (elabTerm x)) []
